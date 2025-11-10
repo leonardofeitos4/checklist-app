1aeda21c7c1b549150eb974f844b1c9402d59406
leonardofeitos4 <leonardofeitos4@gmail.com>
2025-11-09 22:27:18 -0300

    atualizaçao frontedn

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -87,7 +87,7 @@ function TaskItem({ task, onToggle, onDelete }) {
             </span>
           </div>
           <div className="mt-1 flex items-center gap-2 text-sm text-gray-600">
-            <Clock className="w-4 h-4 opacity-80" />
+            <CalendarPlus className="w-5 h-5" />
             <span>{formatDate(task.date, task.time)}</span>
           </div>
         </div>
