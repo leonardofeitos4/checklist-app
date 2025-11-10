6c8a17fd45ee93f33f6db2cf99682b22595e367e
leonardofeitos4 <leonardofeitos4@gmail.com>
2025-11-10 11:59:37 -0300

    config

--- src/config/googleConfig.js
+++ src/config/googleConfig.js
@@ -1,5 +1,5 @@
 export const GOOGLE_CONFIG = {
-  CLIENT_ID: '383684720912-fvltfj2ugq4qvgqh4pnbcca283ckvqu2.apps.googleusercontent.com',
+  CLIENT_ID: '738142740619-241uaep15g4v4nlsobfar9hbjgu9htl0.apps.googleusercontent.com',
   API_KEY: '', // pode deixar vazio se não tiver configurado
   SCOPES: 'https://www.googleapis.com/auth/calendar.events',
   DISCOVERY_DOCS: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest']
